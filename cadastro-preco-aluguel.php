153615690380e4f07a94dc521b05bea2533feb54
Guilherme <guilhermeztt26@gmail.com>
2017-05-12 20:30:38 -0300

    atualização nos cadastros de preço

--- cadastro-preco-aluguel.php
+++ cadastro-preco-aluguel.php
@@ -2,15 +2,18 @@
 
 include "includes/conexao.php";
 
-$id_aluguel = $_POST["id_aluguel"];
+$tipo_aluguel = $_POST["tipo_aluguel"];
 $preco_aluguel = $_POST["preco_aluguel"];
 $data_validade = $_POST["rg_funcionario"];
+
+$aluguel = mysqli_query($conexao, "SELECT id_aluguel FROM aluguel WHERE tipo_aluguel = '$tipo_aluguel'");
+$id_aluguel = mysqli_fetch_assoc($aluguel)['id_aluguel'];
 
 mysqli_query($conexao, "SELECT id_aluguel FROM preco_aluguel WHERE id_aluguel = '$id_aluguel'");
 if(mysqli_affected_rows($conexao) == 0)
 {
-	mysqli_query($conexao, "INSERT INTO preco_aluguel VALUES ('$id_aluguel','$preco_aluguel','$data_validade')");
-	if(mysqli_affected_rows($conexao) == 1)
+	$result1 = mysqli_query($conexao, "INSERT INTO preco_aluguel (id_aluguel, preco_aluguel, data_validade) VALUES ('$id_aluguel','$preco_aluguel','$data_validade')");
+	if($result1)
 		echo "Preço do aluguel cadastrado com sucesso!";
 	else
 		echo "Erro no cadastro do preço do aluguel";
@@ -20,11 +23,11 @@ else
 	$dt = mysqli_query($conexao, "SELECT data_validade FROM preco_aluguel WHERE data_validade > $data_validade");
 	if(mysqli_affected_rows($conexao) == 0)
 	{
-	mysqli_query($conexao, "INSERT INTO preco_aluguel VALUES ('$id_aluguel','$preco_aluguel','$data_validade')");
-	if(mysqli_affected_rows($conexao) == 1)
-		echo "Preço do aluguel cadastrado com sucesso!";
-	else
-		echo "Existe um preço cadastrado com data de validade posterior a tentativa de cadastro. Por favor, cadastre um preço para este aluguel com data superior à $dt";
+		$result2 = mysqli_query($conexao, "INSERT INTO preco_aluguel (id_aluguel, preco_aluguel, data_validade) VALUES ('$id_aluguel','$preco_aluguel','$data_validade')");
+		if($result2)
+			echo "Preço do aluguel cadastrado com sucesso!";
+		else
+			echo "Existe um preço cadastrado com data de validade posterior a tentativa de cadastro. Por favor, cadastre um preço para este aluguel com data superior à $dt";
 	}
 }
 
--- cadastro-preco-servico.php
+++ cadastro-preco-servico.php
@@ -2,15 +2,18 @@
 
 include "includes/conexao.php";
 
-$id_servico = $_POST["id_servico"];
+$tipo_servico = $_POST["tipo_servico"];
 $preco_servico = $_POST["preco_servico"];
 $data_validade = $_POST["rg_funcionario"];
+
+$servico = mysqli_query($conexao, "SELECT id_servico FROM servico WHERE tipo_servico = '$tipo_servico'"))
+$id_servico = mysqli_fetch_assoc($servico)['id_servico'];
 
 mysqli_query($conexao, "SELECT id_servico FROM preco_servico WHERE id_servico = '$id_servico'");
 if(mysqli_affected_rows($conexao) == 0)
 {
-	mysqli_query($conexao, "INSERT INTO preco_servico VALUES ('$id_servico','$preco_servico','$data_validade')");
-	if(mysqli_affected_rows($conexao) == 1)
+	$result1 = mysqli_query($conexao, "INSERT INTO preco_servico (id_servico, preco_servico, data_validade) VALUES ('$id_servico','$preco_servico','$data_validade')");
+	if($result1)
 		echo "Preço do serviço cadastrado com sucesso!";
 	else
 		echo "Erro no cadastro do preço do serviço";
@@ -20,11 +23,11 @@ else
 	$dt = mysqli_query($conexao, "SELECT data_validade FROM preco_servico WHERE data_validade > $data_validade");
 	if(mysqli_affected_rows($conexao) == 0)
 	{
-	mysqli_query($conexao, "INSERT INTO preco_aluguel VALUES ('$id_servico','$preco_servico','$data_validade')");
-	if(mysqli_affected_rows($conexao) == 1)
-		echo "Preço do serviço cadastrado com sucesso!";
-	else
-		echo "Existe um preço cadastrado com data de validade posterior a tentativa de cadastro. Por favor, cadastre um preço para este serviço com data superior à $dt";
+		$result2 = mysqli_query($conexao, "INSERT INTO preco_servico (id_servico, preco_servico, data_validade) VALUES ('$id_servico','$preco_servico','$data_validade')");
+		if($result2)
+			echo "Preço do serviço cadastrado com sucesso!";
+		else
+			echo "Existe um preço cadastrado com data de validade posterior a tentativa de cadastro. Por favor, cadastre um preço para este serviço com data superior à $dt";
 	}
 }
 
