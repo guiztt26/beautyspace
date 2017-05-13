a29b3895a841c61c2c9285ded818651e66848b64
Guilherme <guilhermeztt26@gmail.com>
2017-05-13 16:21:23 -0300

    alteração no cadastro do preços

--- cadastro-preco-servico.php
+++ cadastro-preco-servico.php
@@ -14,22 +14,22 @@ if(mysqli_affected_rows($conexao) == 1)
 {
 	$result1 = mysqli_query($conexao, "INSERT INTO preco_servico (id_servico, preco_servico, data_validade) VALUES ('$id_servico','$preco_servico','$data_validade')");
 	if($result1)
-		echo "Preço do serviço cadastrado com sucesso!";
+	{
+		$dt = mysqli_query($conexao, "SELECT data_validade FROM preco_servico WHERE data_validade > '$data_validade'");
+		if(mysqli_affected_rows($conexao) == 0)
+		{
+			$result2 = mysqli_query($conexao, "INSERT INTO preco_servico (id_servico, preco_servico, data_validade) VALUES ('$id_servico','$preco_servico','$data_validade')");
+			if($result2)
+				echo "Preço do serviço cadastrado com sucesso!";
+			else
+				echo "Existe um preço cadastrado com data de validade posterior a tentativa de cadastro. Por favor, cadastre um preço para este serviço com data superior à $dt";
+		}
+	}
 	else
 		echo "Erro no cadastro do preço do serviço";
 }
 else
-{
-	$dt = mysqli_query($conexao, "SELECT data_validade FROM preco_servico WHERE data_validade > $data_validade");
-	if(mysqli_affected_rows($conexao) == 0)
-	{
-		$result2 = mysqli_query($conexao, "INSERT INTO preco_servico (id_servico, preco_servico, data_validade) VALUES ('$id_servico','$preco_servico','$data_validade')");
-		if($result2)
-			echo "Preço do serviço cadastrado com sucesso!";
-		else
-			echo "Existe um preço cadastrado com data de validade posterior a tentativa de cadastro. Por favor, cadastre um preço para este serviço com data superior à $dt";
-	}
-}
+	echo "Não existe um serviço cadastrado com este nome. Por favor, verifique se digitou o nome corretamente ou cadastre o serviço desejado."
 
 mysqli_close($conexao);
 ?>
