92153121c6c8f890ab56e49b039a1d287e26c49b
Guilherme <guilhermeztt26@gmail.com>
2017-05-13 16:39:37 -0300

    atualização cadastro de preços

--- cadastro-preco-aluguel.php
+++ cadastro-preco-aluguel.php
@@ -12,24 +12,20 @@ $id_aluguel = mysqli_fetch_assoc($aluguel)['id_aluguel'];
 mysqli_query($conexao, "SELECT id_aluguel FROM preco_aluguel WHERE id_aluguel = '$id_aluguel'");
 if(mysqli_affected_rows($conexao) == 1)
 {
-	$result1 = mysqli_query($conexao, "INSERT INTO preco_aluguel (id_aluguel, preco_aluguel, data_validade) VALUES ('$id_aluguel','$preco_aluguel','$data_validade')");
-	if($result1)
+	$dt = mysqli_query($conexao, "SELECT data_validade FROM preco_aluguel WHERE data_validade > '$data_validade'");
+	if(mysqli_affected_rows($conexao) == 0)
 	{
-		$dt = mysqli_query($conexao, "SELECT data_validade FROM preco_aluguel WHERE data_validade > '$data_validade'");
-		if(mysqli_affected_rows($conexao) == 0)
-		{
-			$result2 = mysqli_query($conexao, "INSERT INTO preco_aluguel (id_aluguel, preco_aluguel, data_validade) VALUES ('$id_aluguel','$preco_aluguel','$data_validade')");
-			if($result2)
-				echo "Preço do aluguel cadastrado com sucesso!";
-			else
-				echo "Existe um preço cadastrado com data de validade posterior a tentativa de cadastro. Por favor, cadastre um preço para este aluguel com data superior à $dt";
-		}
+		$result1 = mysqli_query($conexao, "INSERT INTO preco_aluguel (id_aluguel, preco_aluguel, data_validade) VALUES ('$id_aluguel','$preco_aluguel','$data_validade')");
+		if($result1)
+			echo "Preço do aluguel cadastrado com sucesso!";
+		else
+			echo "Existe um preço cadastrado com data de validade posterior a tentativa de cadastro. Por favor, cadastre um preço para este aluguel com data superior à $dt";
 	}
 	else
 		echo "Erro no cadastro do preço do aluguel";
 }
 else
-	echo "Não existe um aluguel cadastrado com este nome. Por favor, verifique se digitou o nome corretamente ou cadastre o aluguel desejado."
+	echo "Não existe um aluguel cadastrado com este nome. Por favor, verifique se digitou o nome corretamente ou cadastre o aluguel desejado.";
 
 mysqli_close($conexao);
 ?>
--- cadastro-preco-servico.php
+++ cadastro-preco-servico.php
@@ -12,18 +12,14 @@ $id_servico = mysqli_fetch_assoc($servico)['id_servico'];
 mysqli_query($conexao, "SELECT id_servico FROM servico WHERE id_servico = '$id_servico'");
 if(mysqli_affected_rows($conexao) == 1)
 {
-	$result1 = mysqli_query($conexao, "INSERT INTO preco_servico (id_servico, preco_servico, data_validade) VALUES ('$id_servico','$preco_servico','$data_validade')");
-	if($result1)
+	$dt = mysqli_query($conexao, "SELECT data_validade FROM preco_servico WHERE data_validade > '$data_validade'");
+	if(mysqli_affected_rows($conexao) == 0)
 	{
-		$dt = mysqli_query($conexao, "SELECT data_validade FROM preco_servico WHERE data_validade > '$data_validade'");
-		if(mysqli_affected_rows($conexao) == 0)
-		{
-			$result2 = mysqli_query($conexao, "INSERT INTO preco_servico (id_servico, preco_servico, data_validade) VALUES ('$id_servico','$preco_servico','$data_validade')");
-			if($result2)
-				echo "Preço do serviço cadastrado com sucesso!";
-			else
-				echo "Existe um preço cadastrado com data de validade posterior a tentativa de cadastro. Por favor cadastre um preço para este serviço com data superior à $dt";
-		}
+		$result1 = mysqli_query($conexao, "INSERT INTO preco_servico (id_servico, preco_servico, data_validade) VALUES ('$id_servico','$preco_servico','$data_validade')");
+		if($result1)
+			echo "Preço do serviço cadastrado com sucesso!";
+		else
+			echo "Existe um preço cadastrado com data de validade posterior a tentativa de cadastro. Por favor cadastre um preço para este serviço com data superior à $dt";
 	}
 	else
 		echo "Erro no cadastro do preço do serviço";
