bdd4341cb1c2b87412defc909466072bfef5d21d
iglesiasufpr <luissanclimentiglesias@gmail.com>
2017-05-15 14:44:55 -0300

    alteração cadastro-contrato-profissional

--- cadastro-contrato-funcionario.php
+++ cadastro-contrato-funcionario.php
@@ -15,16 +15,16 @@ mysqli_query($conexao, "SELECT cpf_funcionario FROM contrato_funcionario WHERE c
 if(mysqli_affected_rows($conexao) == 1)
 {
 	$dt = mysqli_query($conexao, "SELECT data_fim FROM contrato_funcionario WHERE data_fim => '$data_inicio'")
-	if (mysqli_affected_rows($conexao) == 0)
+	if (mysqli_affected_rows($conexao) == 1)
 	{
 		mysqli_query($conexao, "INSERT INTO contrato_funcionario (cpf_funcionario, id_cargo, descricao, data_inicio, data_fim) VALUES ('$cpf_funcionario', '$id_cargo', '$descricao_contrato', '$data_inicio', '$data_fim')");
 		if(mysqli_affected_rows($conexao) == 1)
 			echo "Contrato cadastrado com sucesso!";
 		else
-			echo "Erro no cadastro do Contrato";
+			echo "Existe um contrato vigente cadastrado para este funcionário. Cadastre um contrato com data inicial posteior a $dt";
 	}
 	else
-		echo "Existe um contrato vigente cadastrado para este funcionário. Cadastre um contrato com data inicial posteior a $dt";
+		echo "Erro no cadastro do Contrato";
 }
 else
 	echo "Este funcionário ainda não está cadastrado. Por favor, verifique se CPF digitado está correto ou cadastre o funcionário.";
--- cadastro-contrato-profissional.php
+++ cadastro-contrato-profissional.php
@@ -2,7 +2,7 @@
 
 include "includes/conexao.php";
 
-$descricao_contrato = $_POST["descricao_contrato"];
+$descricao_contrato = $_POST["descricao"];
 $data_inicio = $_POST["data_inicio"];
 $data_fim = $_POST["data_fim"];
 $cpf_profissional = $_POST["cpf_profissional"];
@@ -15,13 +15,12 @@ $id_aluguel = mysqli_fetch_assoc($aluguel)['id_aluguel'];
 $servico = mysqli_query($conexao, "SELECT id_servico FROM servico WHERE tipo_servico = '$tipo_servico'");
 $id_servico = mysqli_fetch_assoc($servico)['id_servico'];
 
-mysqli_query($conexao, "SELECT cpf_profissional FROM contrato_profissional WHERE cpf_profissional = '$cpf_profissional'");
+mysqli_query($conexao, "SELECT cpf_profissional FROM profissional_estetica WHERE cpf_profissional = '$cpf_profissional'");
 if(mysqli_affected_rows($conexao) == 1)
 {
-	$data = mysqli_query($conexao, "SELECT data_fim FROM contrato_profissional WHERE data_fim => '$data_inicio'");
+	$data = mysqli_query($conexao, "SELECT data_fim FROM contrato_profissional WHERE data_fim >= '$data_inicio' and cpf_profissional = '$cpf_profissional' ORDER BY id_contrato DESC");
 	if (mysqli_affected_rows($conexao) == 0)
 	{
-		$dt = mysqli_fetch_assoc($data)['data_fim'];
 		$result1 = mysqli_query($conexao, "INSERT INTO contrato_profissional (descricao, data_inicio, data_fim, cpf_profissional, id_aluguel) VALUES ('$descricao_contrato', '$data_inicio', '$data_fim', '$cpf_profissional', '$id_aluguel')");
 		if($result1)
 		{
@@ -30,14 +29,19 @@ if(mysqli_affected_rows($conexao) == 1)
 			if($result2)
 				echo "Contrato cadastrado com sucesso!";
 			else
-				mysqli_query($conexa, "DELETE FROM contrato_profissional WHERE id_contrato = '$id_contrato'");
-				echo "Erro no cadastro do Contrato";	
+			{
+				mysqli_query($conexao, "DELETE FROM contrato_profissional WHERE id_contrato = '$id_contrato'");
+				echo "Erro no cadastro do Contrato";
+			}	
 		}
 		else
-		echo "Erro no cadastro do Contrato";
+			echo "Erro no cadastro do Contrato";
 	}
 	else
+	{
+		$dt = mysqli_fetch_assoc($data)['data_fim'];
 		echo "Existe um contrato vigente cadastrado para este profissional. Cadastre um contrato com data inicial posteior a $dt";
+	}
 }
 else
 	echo "CPF não encontrado. Por favor, verifique se digitou o CPF corretamente ou cadastre o profissional de estética.";
